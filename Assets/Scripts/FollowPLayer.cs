42f9399fcdead5f805ddc91dbcaa6956ce6d582e
jameson1234 <jg01626@srcs.org>
2020-12-02 14:30:13 -0800

    Tutorials complete

---
+++ Assets/Scripts/FollowPLayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPLayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset + new Vector3(0, 5, -7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.position = player.transform.postition + offset; 
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = InputGetAxis("Horizontal")
+        forwardInput = forwardInput.GetAxis("Vertical")
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.foward * Time.DeltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.DeltaTime );
+
+    }
+}
